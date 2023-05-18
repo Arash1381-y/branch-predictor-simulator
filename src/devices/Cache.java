e86eaa994a3608025f709c206b31bd66c52bf3cb
halfblood <arashyadegari0402@gmail.com>
2023-05-19 00:54:19 +0330

    add puIfAbsent method and rename the getOrDefault to setDefault for the sake of clarity

--- src/devices/Cache.java
+++ src/devices/Cache.java
@@ -21,15 +21,25 @@ public interface Cache<K, V> extends Monitor {
      */
     void put(K key, V value);
 
+
     /**
-     * helper function which calls get and put of the class to read and write, if needed, in the cache.
+     * map a value to the key if the key is not found in the cache
+     *
+     * @param key the address which is selected to put the data in it
+     * @param value the data which is saved in address (key) if the key is not mapped to any not-null data
+     */
+    void putIfAbsent(K key, V value);
+
+    /**
+     * return the value associated with the key. If no value is associated to the key then set the default value
+     * in it.
      *
      * @param key the address
      * @param defaultValue default value if the address is not associated with any block in cache
      * @return the value which is saved in the address. if the value is not set then write default value in cache
      * and return it
      */
-    V getOrDefault(K key, V defaultValue);
+    V setDefault(K key, V defaultValue);
 
     /**
      * clear the cache or simply remove all the entries
