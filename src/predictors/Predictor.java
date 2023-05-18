26c391ee77f80acc82def88692efa48613ee2bf1
halfblood <arashyadegari0402@gmail.com>
2023-05-18 11:29:09 +0330

    modify the parameters

--- src/predictors/Predictor.java
+++ src/predictors/Predictor.java
@@ -1,11 +1,12 @@
 package predictors;
 
 
+import utils.Bit;
 import utils.BranchPredicationResult;
 
 public interface Predictor {
 
-    public BranchPredicationResult predict(boolean[] PC);
+    public BranchPredicationResult predict(Bit[] PC);
 
 }
 
