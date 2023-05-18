1c4f9535b2060b27a14b8e7037fcf2244d2886aa
halfblood <arashyadegari0402@gmail.com>
2023-05-18 19:04:03 +0330

    override toString method for monitoring

--- src/utils/Bit.java
+++ src/utils/Bit.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.StringJoiner;
+
 public enum Bit {
     ZERO(false),
     ONE(true);
@@ -12,5 +14,18 @@ public enum Bit {
 
     public boolean getValue() {
         return this.value;
+    }
+
+    @Override
+    public String toString() {
+        return value ? "1" : "0";
+    }
+
+    public static String bitArrayToString(Bit[] bitArray) {
+        StringJoiner sj = new StringJoiner("", "", "");
+        for (Bit bit : bitArray) {
+            sj.add(bit.toString());
+        }
+        return sj.toString();
     }
 }
