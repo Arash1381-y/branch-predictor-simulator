3960b9b8815051c38cd606894dac9e57a56a0539
halfblood <arashyadegari0402@gmail.com>
2023-05-19 10:38:39 +0330

    refactor documents

--- src/devices/Cache.java
+++ src/devices/Cache.java
@@ -16,7 +16,7 @@ public interface Cache<K, V> extends Monitor {
     V get(K key);
 
     /**
-     * @param key   the address which is selected to put the data in it
+     * @param key   the address which the data is associated with
      * @param value the data which is saved in address (key)
      */
     void put(K key, V value);
@@ -25,14 +25,14 @@ public interface Cache<K, V> extends Monitor {
     /**
      * map a value to the key if the key is not found in the cache
      *
-     * @param key the address which is selected to put the data in it
-     * @param value the data which is saved in address (key) if the key is not mapped to any not-null data
+     * @param key the address which the data is associated with
+     * @param value the data which is mapped to address (key) if the key is not mapped to any not-null data
      */
     void putIfAbsent(K key, V value);
 
     /**
      * return the value associated with the key. If no value is associated to the key then set the default value
-     * in it.
+     * as block value.
      *
      * @param key the address
      * @param defaultValue default value if the address is not associated with any block in cache
