29833f9303b347f742b230ddd6df8ee644f8b782
halfblood <arashyadegari0402@gmail.com>
2023-05-30 15:49:24 +0330

    Add `toNumber` method and rename the `bitArrayToString` to `ArrayToString`

--- src/utils/Bit.java
+++ src/utils/Bit.java
@@ -1,6 +1,8 @@
 package utils;
 
+import java.util.Arrays;
 import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
 public enum Bit {
     ZERO(false),
@@ -8,10 +10,26 @@ public enum Bit {
 
     private final boolean value;
 
-    private Bit(boolean value) {
+    Bit(boolean value) {
         this.value = value;
     }
 
+    /**
+     * Converts the current value of the saturating counter to an integer.
+     * The most significant bit of the counter is assumed to be the leftmost bit
+     * in the register array.
+     *
+     * @return the integer value of the saturating counter
+     */
+    public static int toNumber(Bit[] array) {
+        int result = 0;
+        for (Bit bit : array) result = (result << 1) | (bit == Bit.ONE ? 1 : 0);
+        return result;
+    }
+
+    /**
+     * @return the value assigned to bit enum
+     */
     public boolean getValue() {
         return this.value;
     }
@@ -21,11 +39,9 @@ public enum Bit {
         return value ? "1" : "0";
     }
 
-    public static String bitArrayToString(Bit[] bitArray) {
-        StringJoiner sj = new StringJoiner("", "", "");
-        for (Bit bit : bitArray) {
-            sj.add(bit.toString());
-        }
-        return sj.toString();
+    public static String arrayToString(Bit[] array) {
+        return Arrays.stream(array)
+                .map(Bit::toString)
+                .collect(Collectors.joining());
     }
 }
